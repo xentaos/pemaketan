a27d9d0ea810430566db63190b90f4b14b0110ac
Dindin Hernawan <root@dev.xentaos.org>
2017-12-01 02:55:28 +0700

    update

--- main/p/paketku.sh
+++ main/p/paketku.sh
@@ -12,6 +12,14 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-git clone git@github.com:xentaos/paketku.git
-cd paketku
-git remote set-url origin git@github.com:xentaos/paketku.git
+package=paketku
+figlet $package
+mkdir $package
+cd $package
+echo " Mengcoloning Paket $package dari Github Xenta OS Repository... "
+git clone git@github.com:xentaos/$package.git
+echo " Pindah Kelokasi $package dari Github Xenta OS Repository... "
+cd $package
+echo " Membuat Remote $package ke Github Xenta OS Repository... "
+git remote set-url origin git@github.com:xentaos/$package.git
+echo " Berhasil... "
